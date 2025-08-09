56a9610189a9752e35f9b07d836f8f57dbc60ca1
felipemelo93 <felipe_melo9318@hotmail.com>
2025-08-09 08:34:55 -0500

    actualizar mi rama

--- lienzoback/src/modules/users/users.service.ts
+++ lienzoback/src/modules/users/users.service.ts
@@ -4,10 +4,6 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
-  }
-
   findAll() {
     return `This action returns all users`;
   }
