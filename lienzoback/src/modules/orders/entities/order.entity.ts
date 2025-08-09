68fc383dd5a56cd23178b05166d1af0e9b755a7b
CMolinaBetancourt <cathemolinab@gmail.com>
2025-08-09 05:53:22 -0700

    product/inactivate seeders/category/product entities/all

---
+++ lienzoback/src/modules/orders/entities/order.entity.ts
@@ -0,0 +1,42 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm';
+import { Users } from 'src/modules/users/entities/user.entity';
+import { OrderDetail } from 'src/modules/orders/entities/order-detail.entity';
+import { DiscountCodesUsed } from 'src/modules/discount-codes/entities/discount-codes-used.entity';
+
+export enum OrderStatus {
+  PENDING = 'pending',
+  SHIPPED = 'shipped',
+  DELIVERED = 'delivered',
+  CANCELED = 'canceled',
+}
+
+@Entity({ name: 'ORDERS' })
+export class Orders {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ type: 'date' })
+  date: Date;
+
+  @Column({ type: 'decimal', precision: 10, scale: 2 })
+  total: number;
+
+  @Column({ name: 'discount_id', type: 'int', nullable: true })
+  discountId: number;
+
+  @Column({ type: 'enum', enum: OrderStatus, default: OrderStatus.PENDING })
+  statusOrder: OrderStatus;
+
+  @Column({ name: 'is_paid', default: false })
+  isPaid: boolean;
+
+  @ManyToOne(() => Users, (user) => user.orders)
+  @JoinColumn({ name: 'user_id' })
+  user: Users;
+
+  @OneToMany(() => OrderDetail, (orderDetail) => orderDetail.order)
+  orderDetails: OrderDetail[];
+
+  @OneToMany(() => DiscountCodesUsed, (discountCodesUsed) => discountCodesUsed.order)
+  discountCodesUsed: DiscountCodesUsed[];
+}
