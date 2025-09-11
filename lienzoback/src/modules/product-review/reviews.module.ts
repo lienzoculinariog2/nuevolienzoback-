5751df379787a94f61d56c38d7b37fe2ba46837f
CMolinaBetancourt <cathemolinab@gmail.com>
2025-09-11 12:14:10 -0700

    reviews

--- lienzoback/src/modules/product-review/reviews.module.ts
+++ lienzoback/src/modules/product-review/reviews.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { Users } from '../users/entities/user.entity';
+import { Reviews } from './entities/review.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Reviews, Users])],
   controllers: [ReviewsController],
   providers: [ReviewsService],
 })
--- lienzoback/src/modules/product-review/reviews.service.ts
+++ lienzoback/src/modules/product-review/reviews.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
-import { UpdateReviewDto } from './dto/update-review.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Reviews } from './entities/review.entity'; // Asume que tu entidad se llama Reviews
+import { Users } from '../users/entities/user.entity'; // Asume que tu entidad se llama Users
 
 @Injectable()
 export class ReviewsService {
-  create(createReviewDto: CreateReviewDto) {
-    return 'This action adds a new review';
+  constructor(
+    @InjectRepository(Reviews)
+    private reviewsRepository: Repository<Reviews>,
+    @InjectRepository(Users)
+    private usersRepository: Repository<Users>,
+  ) {}
+
+  async create(createReviewDto: CreateReviewDto): Promise<Reviews> {
+    const user = await this.usersRepository.findOne({ where: { id: createReviewDto.userId } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${createReviewDto.userId} not found`);
+    }
+
+    const newReview = this.reviewsRepository.create({
+      comment: createReviewDto.comment,
+      rating: createReviewDto.rating,
+      user: user,
+    });
+
+    return this.reviewsRepository.save(newReview);
   }
 
-  findAll() {
-    return `This action returns all reviews`;
+  async findAll(): Promise<Reviews[]> {
+    return this.reviewsRepository.find({ relations: ['user'] });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
+  async findByUserId(userId: string): Promise<Reviews[]> {
+    const user = await this.usersRepository.findOne({ where: { id: userId } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${userId} not found`);
+    }
+    return this.reviewsRepository.find({ where: { user: { id: userId } }, relations: ['user'] });
   }
 
-  update(id: number, updateReviewDto: UpdateReviewDto) {
-    return `This action updates a #${id} review`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} review`;
+  async remove(id: string): Promise<{ message: string }> {
+    const result = await this.reviewsRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Review with ID ${id} not found`);
+    }
+    return { message: `Review with ID ${id} successfully deleted` };
   }
 }
